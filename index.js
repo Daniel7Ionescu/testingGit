a5c4f9682d20e53ffecd93cd8f739c38ba990ddd
Cristian Daniel Ionescu <inb3zna@gmail.com>
2023-03-08 17:57:19 +0200

    added cow stuff

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-let greetEl = document.getElementById('greet-el');
-greetEl.textContent = 'Hi! Stuff will be here!';
+let nameEl = document.getElementById('name-el');
+nameEl.textContent = 'Cow stuff';
+
+let cowEl = document.getElementById('cow-el');
+
+function moo(){
+    cowEl.textContent = "";
+    cowEl.textContent = "Moo!";
+}
+function oink(){
+    cowEl.textContent = "";
+    cowEl.textContent = "Oink!";
+}
+function meaw(){
+    cowEl.textContent = "";
+    cowEl.textContent = "Meaw!";
+}
